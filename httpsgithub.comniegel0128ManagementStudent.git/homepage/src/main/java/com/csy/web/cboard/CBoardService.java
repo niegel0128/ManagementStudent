06ee88c643cad51a0d55051fdf99a7acb698672a
niegel0128 <niegel0128@gmail.com>
2016-05-12 10:14:53 +0900

    회원가입 완료 ajax처리 이전임

--- httpsgithub.comniegel0128ManagementStudent.git/homepage/src/main/java/com/csy/web/cboard/CBoardService.java
+++ httpsgithub.comniegel0128ManagementStudent.git/homepage/src/main/java/com/csy/web/cboard/CBoardService.java
@@ -5,7 +5,9 @@ import java.util.List;
 import com.csy.web.admin.AdminBean;
 
 public interface CBoardService {
+	
 	public int add(CBoardBean cBoardBean);
+	//메인 데이터 출력
 	public List<CBoardBean> list();
 	public List<CBoardBean> getByName(CBoardBean cBoardBean);
 	public AdminBean getById(CBoardBean cBoardBean);
