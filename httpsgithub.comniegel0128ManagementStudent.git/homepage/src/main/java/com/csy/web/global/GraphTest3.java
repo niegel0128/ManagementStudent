807345c58ecd7143fdcb3bede08a696c72ccb8c6
niegel0128 <niegel0128@gmail.com>
2016-05-23 14:31:13 +0900

    구글차트 작업중

--- httpsgithub.comniegel0128ManagementStudent.git/homepage/src/main/java/com/csy/web/global/GraphTest3.java
+++ httpsgithub.comniegel0128ManagementStudent.git/homepage/src/main/java/com/csy/web/global/GraphTest3.java
@@ -7,43 +7,40 @@ public class GraphTest3 {
 public static void main(String[] args) {
 	JSONObject ajaxObjCols1 = new JSONObject();    
 	JSONObject ajaxObjCols2 = new JSONObject();    
-	JSONObject ajaxObjCols3 = new JSONObject();    
-	JSONObject ajaxObjCols4 = new JSONObject();    
 	
 	JSONArray ajaxArrayCols1 = new JSONArray();    
+	JSONArray ajaxArrayCols2 = new JSONArray();    
 	
 	ajaxObjCols1.put("id", "");
-	ajaxObjCols2.put("label", "Topping");
-	ajaxObjCols2.put("pattern", "");
-	ajaxObjCols2.put("type", "string");
+	ajaxObjCols1.put("label", "Topping");
+	ajaxObjCols1.put("pattern", "");
+	ajaxObjCols1.put("type", "string");
 	
 	ajaxArrayCols1.add(ajaxObjCols1);
-	ajaxArrayCols1.add(ajaxObjCols2);
-	ajaxArrayCols1.add(ajaxObjCols3);
-	ajaxArrayCols1.add(ajaxObjCols4);
 	
-	System.out.println(ajaxArrayCols1);
+	ajaxObjCols2.put("id", "");
+	ajaxObjCols2.put("label", "Slices");
+	ajaxObjCols2.put("pattern", "");
+	ajaxObjCols2.put("type", "number");
+	
+	ajaxArrayCols2.add(ajaxObjCols2);
+	
+	//System.out.println(ajaxArrayCols1);
+	//System.out.println(ajaxArrayCols2);
 	
 	
-	JSONObject data = new JSONObject();
 	JSONObject ajaxObjRows1 = new JSONObject();    
 	JSONObject ajaxObjRows2 = new JSONObject();   
-	JSONArray ajaxArrayRows4 = new JSONArray();   
-	JSONObject ajaxObjRows5 = new JSONObject();   
 	
-	
-	
-	JSONObject ajaxObjRows3 = new JSONObject();    
+	JSONObject ajaxObjRows3 = new JSONObject();   
 	
 	JSONArray ajaxArryRows1 = new JSONArray();        
 	
-	ajaxObjRows1.put("v", "2014-08-27 16:13:17");
-	ajaxObjRows2.put("v", "294");
+	ajaxObjRows1.put("v", "Mushrooms");
+	ajaxObjRows1.put("f", null);
+	ajaxObjRows2.put("v", 3);
+	ajaxObjRows2.put("f", null);
 	
-	
-	String ajaxObjRows1result = ajaxObjRows2.toString();
-	String ajaxObjRows2result = ajaxObjRows2.toString();
-
 	ajaxArryRows1.add(ajaxObjRows1);
 	ajaxArryRows1.add(ajaxObjRows2);
 	
@@ -57,7 +54,7 @@ public static void main(String[] args) {
 	
 	ajaxArryRows2.add(ajaxObjRows3);
 	
-	System.out.println(ajaxArryRows2);
+	//System.out.println(ajaxArryRows2);
 	
 	
 	JSONObject ajaxObjColsRows1 = new JSONObject();
--- httpsgithub.comniegel0128ManagementStudent.git/homepage/src/main/java/com/csy/web/graph/GraphController.java
+++ httpsgithub.comniegel0128ManagementStudent.git/homepage/src/main/java/com/csy/web/graph/GraphController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.csy.web.global.GlobalController;
+import com.google.gson.Gson;
 
 @Controller
 @RequestMapping(value = "/graph")
@@ -24,30 +25,38 @@ public class GraphController {
 		JSONObject ajaxObjCols2 = new JSONObject();    
 		
 		JSONArray ajaxArrayCols1 = new JSONArray();    
+		JSONArray ajaxArrayCols2 = new JSONArray();    
 		
-		ajaxObjCols1.put("type", "string");
-		ajaxObjCols2.put("type", "number");
+		ajaxObjCols1.put("id", "");
+		//ajaxObjCols1.put("label", "Topping");
+		ajaxObjCols1.put("pattern", "");
+		//ajaxObjCols1.put("type", "string");
 		
 		ajaxArrayCols1.add(ajaxObjCols1);
-		ajaxArrayCols1.add(ajaxObjCols2);
 		
-		System.out.println(ajaxArrayCols1);
+		ajaxObjCols2.put("id", "");
+		//ajaxObjCols2.put("label", "Slices");
+		ajaxObjCols2.put("pattern", "");
+		//ajaxObjCols2.put("type", "number");
+		
+		ajaxArrayCols2.add(ajaxObjCols2);
+		
+		//System.out.println(ajaxArrayCols1);
+		//System.out.println(ajaxArrayCols2);
 		
 		
-		JSONObject data = new JSONObject();
 		JSONObject ajaxObjRows1 = new JSONObject();    
 		JSONObject ajaxObjRows2 = new JSONObject();   
 		
-		JSONObject ajaxObjRows3 = new JSONObject();    
+		JSONObject ajaxObjRows3 = new JSONObject();   
 		
 		JSONArray ajaxArryRows1 = new JSONArray();        
 		
-		ajaxObjRows1.put("v", "2014-08-27 16:13:17");
-		ajaxObjRows2.put("v", "294");
+		ajaxObjRows1.put("v", "Mushrooms");
+		//ajaxObjRows1.put("f", null);
+		ajaxObjRows2.put("v", 3);
+		//ajaxObjRows2.put("f", null);
 		
-		String ajaxObjRows1result = ajaxObjRows2.toString();
-		String ajaxObjRows2result = ajaxObjRows2.toString();
-
 		ajaxArryRows1.add(ajaxObjRows1);
 		ajaxArryRows1.add(ajaxObjRows2);
 		
@@ -61,8 +70,7 @@ public class GraphController {
 		
 		ajaxArryRows2.add(ajaxObjRows3);
 		
-		System.out.println(ajaxArryRows2);
-		
+		//System.out.println(ajaxArryRows2);
 		
 		JSONObject ajaxObjColsRows1 = new JSONObject();
 		ajaxObjColsRows1.put("cols", ajaxArrayCols1);
@@ -70,10 +78,15 @@ public class GraphController {
 		
 		System.out.println(ajaxObjColsRows1);
 		
-		String result = ajaxObjColsRows1.toString();  // JSONObject를 String 에 담기
+		//String result = ajaxObjColsRows1.toString();  // JSONObject를 String 에 담기
+		
+		Gson gson = new Gson();
+		String result = gson.toJson(ajaxObjColsRows1); 
+		result.toString();
 		System.out.print(result);
 		
 		model.addAttribute(result);
+		
 		return model;
 	}
 }
